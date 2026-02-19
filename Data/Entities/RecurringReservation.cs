9854a0767f2ab84b6155aa237bf13fd984816362
under-construction <66276837+under-construction@users.noreply.github.com>
2026-02-19 15:05:55 +0300

    number of weeks property for RecurringReservation entity was added

--- Data/Entities/RecurringReservation.cs
+++ Data/Entities/RecurringReservation.cs
@@ -8,6 +8,9 @@ public class RecurringReservation : BaseEntity
     public DateTime Start { get; set; }
     [Required(ErrorMessage = "End date is required.")]
     public DateTime End { get; set; }
+    [Required(ErrorMessage = "Number of weeks is required.")]
+    [Range(1, 8, ErrorMessage = "Recurring reservations can not be made for more than 8 weeks.")]
+    public int NumberOfWeeks { get; set; }
 
     [Required(ErrorMessage = "Room is required.")]
     public Guid RoomId { get; set; }
