422d33fa2cb0c4ef28b6e95bf55696c03811188a
under-construction <66276837+under-construction@users.noreply.github.com>
2026-02-19 21:20:22 +0300

    reservation controller was added

--- API/Controllers/ReservationController.cs
+++ API/Controllers/ReservationController.cs
@@ -26,7 +26,7 @@ public class ReservationController(AppDbContext _context) : ControllerBase
             })
             .ToListAsync();
 
-        return Ok(new { reservations, message = "Load successful." });
+        return Ok(reservations);
     }
 
     [HttpGet("{id}")]
@@ -49,5 +49,86 @@ public class ReservationController(AppDbContext _context) : ControllerBase
             return NotFound("Reservation not found.");
 
         return Ok(reservation);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Reservation>> CreateReservation(ReservationDTO dto)
+    {
+        if (dto == null)
+            return BadRequest("Room data is required.");
+
+        var newReservation = new Reservation
+        {
+            Start = dto.Start,
+            End = dto.End,
+            RoomId = dto.RoomId,
+            UserId = dto.UserId,
+            RecurringReservationId = dto.RecurringReservationId
+        };
+
+        _context.Reservations.Add(newReservation);
+        await _context.SaveChangesAsync();
+
+        var createdReservation = await _context.Reservations
+            .Include(r => r.Room)
+            .Include(r => r.User)
+            .Where(r => r.Id == newReservation.Id)
+            .Select(r => new ReservationResponseDTO
+            {
+                Id = r.Id,
+                Start = r.Start,
+                End = r.End,
+                Room = new() { Id = r.RoomId, Name = r.Room.Name },
+                User = new() { Id = r.UserId, Name = r.User.Name }
+            })
+            .FirstOrDefaultAsync();
+
+        if (createdReservation != null)
+        {
+            return CreatedAtAction(
+               nameof(GetReservation),
+               new { id = newReservation.Id },
+               createdReservation
+           );
+        }
+
+        return BadRequest("Wrong object structure.");
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateReservation(Guid id, ReservationDTO dto)
+    {
+        if (id != dto.Id)
+            return BadRequest("ID mismatch.");
+
+        var reservation = await _context.Reservations
+            .Include(r => r.Room)
+            .Include(r => r.User)
+            .FirstOrDefaultAsync(r => r.Id == id);
+
+        if (reservation == null)
+            return NotFound($"Reservation with Id {id} not found.");
+
+        reservation.Start = dto.Start;
+        reservation.End = dto.End;
+        reservation.RoomId = dto.RoomId;
+        reservation.UserId = dto.UserId;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReservation(Guid id)
+    {
+        var reservation = await _context.Reservations.FindAsync(id);
+
+        if (reservation == null)
+            return NotFound();
+
+        _context.Reservations.Remove(reservation);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
--- Data/DTOs/ReservationDTO.cs
+++ Data/DTOs/ReservationDTO.cs
@@ -4,9 +4,9 @@ public class ReservationDTO : BaseDTO
 {
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
-    public Guid? RoomId { get; set; }
+    public Guid RoomId { get; set; }
     public RoomDTO? Room { get; set; }
-    public Guid? UserId { get; set; }
+    public Guid UserId { get; set; }
     public UserDTO? User { get; set; }
     public Guid? RecurringReservationId { get; set; }
 }
